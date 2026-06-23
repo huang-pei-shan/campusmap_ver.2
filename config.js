5174a3936306341b6bb02422d839484fd57ad640
huang-pei-shan <11244007@gms.tcu.edu.tw>
2026-06-23 23:48:32 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// 將此檔案另存為 config.js 並填入你的值（放在 index.html 同目錄）
+// 注意：如果你把 config.js 提交到公開 repo，檔案內容會公開。
+// 對於 GitHub Pages，通常會把這種檔案加入 .gitignore 或不放入 repo。
+// export const SUPABASE_URL = 'https://mfljkyvdadxlrbxlboce.supabase.co';
+// export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mbGpreXZkYWR4bHJieGxib2NlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MTQwMDUsImV4cCI6MjA4MDM5MDAwNX0.Z4OeacVpO8yM1d1uOWZ6jU2Gl7wgEbhXvAFSqF5pBRs';
+
+// config.js
+// ⚠ 記得改成你自己的 Supabase 專案 URL 與 anon 公鑰
+const SUPABASE_URL = "https://hhczzxukbsmcwlpucbfw.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhoY3p6eHVrYnNtY3dscHVjYmZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMTI3MzksImV4cCI6MjA5Nzc4ODczOX0.kAe9kaCsZrwHAo2OdIwtE1O91q_dRUPXeRlVc_ZB0X8";
